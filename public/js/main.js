727f258b71cdc247f0c8db94827a064d14359c88
David Edwards <Davidjeedwards@gmail.com>
2020-06-01 13:29:06 +0400

    resize working sort of

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,29 @@ const socket = io();
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+
 const eng_display = document.querySelector('.eng-display');
+
 const pilot_display = document.querySelector('.pilot-display');
+const pilot_container = document.querySelector('.pil-container');
 const chat_display = document.querySelector('.chat-display');
 const shared_display = document.querySelector('.shared-display');
+
 const roomTime = document.getElementById('roomTime');
+let btn = document.getElementById('sizeButton');
+
+
+let screenSize = document.querySelector('#screenSize');
+let windowSize = document.querySelector('#windowSize ');
+let contentSize = document.querySelector('#contentSize');
+
+let eng_container = document.querySelector('.eng-container');
+
 var role = 0;
 var time = 0;
 
-
+var window_width = window.innerWidth;
+var window_height = window.innerHeight;
 
 
 //  Get username and room from URL
@@ -114,12 +128,19 @@ function myFunction() {
     const msg = "Start game";
         socket.emit('startGame', msg);
         console.log(msg);
+
+        //engContainer.width = window.innerWidth;
+        //engContainer.height = window.innerHeight;
+
     }
 
     if (role === "Pilot") {
         pilot_display.style.display = "block";
         chat_display.style.display = "none";
         shared_display.style = "block";
+
+        pilot_container.width = window.innerWidth;
+        pilot_container.height = window.innerHeight;
     }
 
 }
@@ -138,8 +159,68 @@ socket.on('update', gameobj => {
 });
 
 
+btn.addEventListener('click', () => {
+    screenSize.innerText = `Screen Height: ${screen.height} - Screen Width: ${screen.width}`
+    windowSize.innerText = `Window Height: ${window.outerHeight} - Window Width: ${window.outerWidth}`
+    contentSize.innerText = `Content Height: ${window.innerHeight} - Content Width: ${window.innerWidth}`
+    console.log("button");
+
+    console.log(eng_container.clientHeight);
+    console.log(eng_container.clientWidth);
+
+    eng_container.setAttribute("style", "display:block;width:500px");
+
+    var width = '500px';
+    var width2 = window.innerWidth + "px";
+    //msg = JSON.stringify(width2);
+    console.log(width2);
+
+    eng_container.style.width = window.innerWidth + "px";
+    eng_container.style.height = window.innerHeight + "px";
+
+})
 
 
 
+// --------------------------------------------------- insure the window size is the right size and set up the game world sizes
 
+window.addEventListener("resize", manageWindowSize);
+
+function manageWindowSize() {
+
+    // 1024x768 is the smallest screen size I need to allow for
+
+    //if ((window.innerWidth > 1024) && window.innerHeight > 768) {
+
+        console.log("The window has been resized!");
+        screenSize.innerText = `Screen Height: ${screen.height} - Screen Width: ${screen.width}`
+        windowSize.innerText = `Window Height: ${window.outerHeight} - Window Width: ${window.outerWidth}`
+        contentSize.innerText = `Content Height: ${window.innerHeight} - Content Width: ${window.innerWidth}`
+        
+
+        // try resizing the container elements
+
+        //window_width = window.innerWidth;
+       // window_height = window.innerHeight;
+
+        //pilot_container.width = window.innerWidth;
+        //pilot_container.height = window.innerHeight;
+
+        //eng_container.width = window.innerWidth;
+        //eng_container.height = window.innerHeight;
+
+}
+
+window.onload = init;
+function init() {
+    if (window.Event) {
+        document.captureEvents(Event.MOUSEMOVE);
+    }
+    document.onmousemove = getCursorXY;
+}
+
+function getCursorXY(e) {
+    document.getElementById('cursorX').value = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+    document.getElementById('cursorY').value = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+}
 
