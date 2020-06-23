2ad04689dd0ddb1c2d71ead80e451bbb2500bc52
David Edwards <Davidjeedwards@gmail.com>
2020-06-23 12:24:55 +0400

    Jq working

--- public/js/main.js
+++ public/js/main.js
@@ -4,254 +4,141 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-const eng_display = document.querySelector('.eng-display');
-
-const pilot_display = document.querySelector('.pilot-display');
-const pilot_container = document.querySelector('.pil-container');
-const chat_display = document.querySelector('.chat-display');
-const shared_display = document.querySelector('.shared-display');
-
-const roomTime = document.getElementById('roomTime');
-let btn = document.getElementById('sizeButton');
-
-
-let screenSize = document.querySelector('#screenSize');
-let windowSize = document.querySelector('#windowSize ');
-let contentSize = document.querySelector('#contentSize');
-
-let eng_container = document.querySelector('.eng-container');
-
-var role = 0;
-var time = 0;
-
-var window_width = window.innerWidth;
-var window_height = window.innerHeight;
-
-var world_width = 100;
-var world_height = 100;
-
 import screenObject from './screenObject.js';
-
-//const screen = screen();
-
-
-//  Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-    ignoreQueryPrefix: true
-});
+var myScreen = new screenObject(screen.width, screen.height);
 
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });            //this is done at the start as soon as a connection is made
 
-//get room and users
-socket.on('roomUsers', ({ room, users }) => {
-    outputRoomName(room);
-    outputUsers(users);
+$(document).ready(function () {
+
+    serverCommunication();
+    setSizeofDom();
+
+
+
 
 });
 
+function serverCommunication() {
 
-//message from server
-socket.on('message', message => {
-    console.log(message);
-    outputMessage(message);
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-// Message submit
-chatForm.addEventListener('submit', e => {
-    e.preventDefault();
-
-    // Get message text
-    const msg = e.target.elements.msg.value;
-
-    // Emit message to server
-    socket.emit('chatMessage', msg);
-
-    // Clear input
-    e.target.elements.msg.value = '';
-    e.target.elements.msg.focus();
-  
-});
+    //  Get username and room from URL
+    const { username, room } = Qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
 
 
-//------------------------------------------------------------------------------------- Output message to DOM
+    // Join chatroom
+    socket.emit('joinRoom', { username, room });            //this is done at the start as soon as a connection is made
 
-function outputMessage(message) {
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    //get room and users
+    socket.on('roomUsers', ({ room, users }) => {
+        outputRoomName(room);
+        outputUsers(users);
+
+    });
+
+
+    //message from server
+    socket.on('message', message => {
+        console.log(message);
+        outputMessage(message);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    });
+
+    // Message submit
+    chatForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        // Get message text
+        const msg = e.target.elements.msg.value;
+
+        // Emit message to server
+        socket.emit('chatMessage', msg);
+
+        // Clear input
+        e.target.elements.msg.value = '';
+        e.target.elements.msg.focus();
+
+    });
+
+
+    function outputMessage(message) {
+        const div = document.createElement('div');
+        div.classList.add('message');
+        div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
   <p class="text">
     ${message.text}
   </p>`;
-    document.querySelector('.chat-messages').appendChild(div);
-}
+        document.querySelector('.chat-messages').appendChild(div);
+    }
 
-// Add room name to DOM
-function outputRoomName(room) {
-    roomName.innerText = room;
-}
+    // Add room name to DOM
+    function outputRoomName(room) {
+        roomName.innerText = room;
+    }
 
-// Add users to DOM
-function outputUsers(users) {
-    userList.innerHTML = `
+    // Add users to DOM
+    function outputUsers(users) {
+        userList.innerHTML = `
     ${users.map(user => `<li>${user.username}</li>`).join('')}
   `;
-}
+    }
 
-function outputTime(time) {
-    roomTime.innerText = time;
-}
-
-//------------------------------------------------------------------------------------------ choose pilot or engineer
-
-document.getElementById("pilot_select").onclick = function () { pilot_function() };
-
-function pilot_function() {
-    role = "Pilot";
-
-    const msg = "Pilot";
-    socket.emit('chatMessage', msg);
 
 }
 
-document.getElementById("engineer_select").onclick = function () { engineer_function() };
+function setSizeofDom() {
+    myScreen.calculateConstants(); 
 
-function engineer_function() {
-    role = "Engineer";
+    setSizeofFont(screen.width);
+    setSizeofDomElements();
 
-    const msg = "Engineer";
-    socket.emit('chatMessage', msg);
-}
 
-document.getElementById("start_game").onclick = function () { start_function() };
+    function setSizeofFont(width) {
 
-function start_function() {
-    // this function is run as soon as the start button is clicked
-    // at this point I want to find out screen information and set the correct div variables to the right size...
+        var fontBody = $(".fontbody");
+        console.log(JSON.stringify(width));
 
-    //function find aspect ratio and width of screen.
+        if (width >= 2048) {
+            fontBody.css("font-size", "22px");
+            $(".fontbutton").html("the font size is 22px");
 
-    var myScreen = new screenObject(screen.width, screen.height);
+        } else if ((2048 > width) && (width >= 1920)) {
+            fontBody.css("font-size", "20px");
+            $(".fontbutton").html("the font size is 20px");
 
-    // I know want to output the aspect ratio:
-    myScreen.calculateConstants();
 
-    console.log(JSON.stringify(myScreen.gcd(1680, 1050)));
+        } else if ((1920 > width) && (width >= 1856)) {
+            fontBody.css("font-size", "16px");
+            $(".fontbutton").html("the font size is 16px");
 
-    if (role === "Engineer") {
-        eng_display.style.display = "block";
-        chat_display.style.display = "none";
-        shared_display.style = "block";
+        } else if ((1856 > width) && (width >= 1200)) {
+            fontBody.css("font-size", "14px");
+            $(".fontbutton").html("the font size is 14px");
+        }
 
-    const msg = "Start game";
-        socket.emit('startGame', msg);
-        console.log(msg);
-
-        //engContainer.width = window.innerWidth;
-        //engContainer.height = window.innerHeight;
 
     }
 
-    if (role === "Pilot") {
-        pilot_display.style.display = "block";
-        chat_display.style.display = "none";
-        shared_display.style = "block";
+    function setSizeofDomElements() {
 
-        pilot_container.width = window.innerWidth;
-        pilot_container.height = window.innerHeight;
+        var containerHeight = 100 * myScreen.hc;
+        var containerWidth = 100 * myScreen.wc;
+        var halfContainerHeight = containerHeight / 2; //+ "px";
+        var halfContainerWidth = containerWidth / 2; //+ "px";
+
+        $(".game-container").css("position", "absolute");
+        $(".game-container").css("top", "50%");
+        $(".game-container").css("left", "50%");
+        $(".game-container").css("margin-top", -halfContainerHeight + "px");
+        $(".game-container").css("margin-left", -halfContainerWidth + "px");
+        $(".game-container").width(containerWidth);
+        $(".game-container").height(containerHeight);
+        $(".game-container").css("border", "2px solid blue");
+        $(".game-container").css("background-color", "white");
+
     }
-
 }
-
-socket.on('starting', message => {
-    console.log("server working");
-
-});
-
-socket.on('update', gameobj => {
-
-    time = JSON.stringify(gameobj.time);
-    outputTime(time);
-   
-
-});
-
-
-btn.addEventListener('click', () => {
-    screenSize.innerText = `Screen Height: ${screen.height} - Screen Width: ${screen.width}`
-    windowSize.innerText = `Window Height: ${window.outerHeight} - Window Width: ${window.outerWidth}`
-    contentSize.innerText = `Content Height: ${window.innerHeight} - Content Width: ${window.innerWidth}`
-    console.log("button");
-
-    console.log(eng_container.clientHeight);
-    console.log(eng_container.clientWidth);
-
-    eng_container.setAttribute("style", "display:block;width:500px");
-
-    var width2 = window.innerWidth + "px";
-    //msg = JSON.stringify(width2);
-    console.log(width2);
-
-    eng_container.style.width = window.innerWidth + "px";
-    eng_container.style.height = window.innerHeight + "px";
-
-    setScreenSize();
-
-})
-
-
-
-// --------------------------------------------------- insure the window size is the right size and set up the game world sizes
-
-window.addEventListener("resize", manageWindowSize);
-
-
-
-function manageWindowSize() {
-
-    // 1024x768 is the smallest screen size I need to allow for
-
-    //if ((window.innerWidth > 1024) && window.innerHeight > 768) {
-
-        console.log("The window has been resized!");
-        screenSize.innerText = `Screen Height: ${screen.height} - Screen Width: ${screen.width}`
-        windowSize.innerText = `Window Height: ${window.outerHeight} - Window Width: ${window.outerWidth}`
-        contentSize.innerText = `Content Height: ${window.innerHeight} - Content Width: ${window.innerWidth}`
-
- 
-
-        // try resizing the container elements
-
-        //window_width = window.innerWidth;
-       // window_height = window.innerHeight;
-
-        //pilot_container.width = window.innerWidth;
-        //pilot_container.height = window.innerHeight;
-
-        //eng_container.width = window.innerWidth;
-        //eng_container.height = window.innerHeight;
-
-}
-
-window.onload = init;
-function init() {
-    if (window.Event) {
-        document.captureEvents(Event.MOUSEMOVE);
-    }
-    document.onmousemove = getCursorXY;
-}
-
-function getCursorXY(e) {
-    document.getElementById('cursorX').value = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-    document.getElementById('cursorY').value = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-}
-
-
-
-//set sizes
-
 
 
 
--- public/js/screenObject.js
+++ public/js/screenObject.js
@@ -1,31 +1,5 @@
 // JavaScript source code
 
-// source code for handlin resize events and insureing that the aspect ratio is set correctly for whatever screen is being used.
-/*
-function gcd(a, b) {
-    return (b == 0) ? a : gcd(b, a % b);
-
-    //will return the lowest common denominator of the width = a and the height = b
-}
-
-export function setScreenSize() {
-    // called at the begining to resize the container appropiatly
-
-    console.log("function called");
-}
-
-function resize() {
-    // called every time a resize event happens
-
-}
-
-function getScreenInformation(w,h) {
-
-
-
-}
-*/
-
 function screenObject(w,h) {
     this.w = 1680;
     this.h = 1050;
@@ -34,7 +8,7 @@ function screenObject(w,h) {
     this.gcdvalue;
 
     //this.gcd;
-    console.log("constructor called");
+    //console.log("constructor called");
 }
 
 screenObject.prototype.gcd = function (a, b) {
@@ -50,14 +24,14 @@ screenObject.prototype.calculateConstants = function () {
     var aw = this.w / gcdv;
     var ah = this.h / gcdv;
 
-    this.wc = this.w / 100 * 0.8;
+    this.wc = this.w / 100 * 0.65;
     this.hc = (this.wc / aw) * ah;
 
     this.wc = Math.round(this.wc * 100) / 100;
     this.hc = Math.round(this.hc * 100) / 100;
 
-    console.log(JSON.stringify(this.wc));
-    console.log(JSON.stringify(this.hc));
+    //console.log(JSON.stringify(this.wc));
+    //console.log(JSON.stringify(this.hc));
 
 }
 
