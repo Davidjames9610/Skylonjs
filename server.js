1cd1382a748409dc97743e51d0b353a58bbc6e5c
David Edwards <Davidjeedwards@gmail.com>
2020-08-25 20:47:49 -0500

    test on heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 
-const PORT = 8090;//process.env.PORT; 
+const PORT = process.env.PORT; //8090;//process.env.PORT; 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
